f5081774dce12b34927d5e0be5019cc1e72448e0
Corgii <corgii123mail@gmail.com>
2023-02-21 16:42:40 +0800

    fix: logo information

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -4,83 +4,37 @@
       <div class="work-space" @click="changeWorkSpace">
         <d-icon name="add-directory" color="#faf0e6"></d-icon>
         <span style="white-space: nowrap; margin-right: 5px">工作目录</span>
-        <d-input
-          v-model="workSpaceFolder"
-          size="sm"
-          placeholder="请选择工作区"
-          readonly
-        ></d-input>
+        <d-input v-model="workSpaceFolder" size="sm" placeholder="请选择工作区" readonly></d-input>
       </div>
       <div class="source-book">
-        <d-icon
-          name="connect-code"
-          color="#faf0e6"
-          style="line-height: 10vh"
-        ></d-icon>
-        <span style="white-space: nowrap; line-height: 10vh; margin-right: 5px"
-          >参考规范材料</span
-        >
-        <div
-          style="
-            display: flex;
-            justify-content: center;
-            width: 100%;
-            flex-direction: column;
-          "
-        >
-          <d-input
-            v-model="Object.keys(sourceBook)[0]"
-            size="sm"
-            readonly
-            @click="openReference(Object.values(sourceBook)[0])"
-            class="reference-input"
-          ></d-input>
-          <d-input
-            v-model="Object.keys(sourceBook)[1]"
-            size="sm"
-            readonly
-            @click="openReference(Object.values(sourceBook)[1])"
-            class="reference-input"
-          ></d-input>
+        <d-icon name="connect-code" color="#faf0e6" style="line-height: 10vh"></d-icon>
+        <span style="white-space: nowrap; line-height: 10vh; margin-right: 5px">参考规范材料</span>
+        <div style="display: flex; justify-content: center; width: 100%; flex-direction: column">
+          <d-input v-model="Object.keys(sourceBook)[0]" size="sm" readonly @click="openReference(Object.values(sourceBook)[0])" class="reference-input"></d-input>
+          <d-input v-model="Object.keys(sourceBook)[1]" size="sm" readonly @click="openReference(Object.values(sourceBook)[1])" class="reference-input"></d-input>
         </div>
       </div>
-      <img :src="logo" alt="" class="logo" />
+      <div class="logo-wrap">
+        <div class="ccs-wrap">
+          <img :src="ccs1" alt="" class="logo" />
+          <img :src="ccs2" alt="" class="logo" style="height: 37%" />
+        </div>
+        <img :src="logo" alt="" class="logo" />
+      </div>
     </header>
     <content class="dcontent">
       <section>
-        <d-splitter
-          class="splitter-border"
-          :orientation="orientation"
-          :splitBarSize="splitBarSize"
-          style="height: 86vh"
-        >
+        <d-splitter class="splitter-border" :orientation="orientation" :splitBarSize="splitBarSize" style="height: 86vh">
           <template v-slot:DSplitterPane>
-            <d-splitter-pane
-              collapseDirection="before"
-              :size="size"
-              :minSize="minSize"
-              :maxSize="maxSize"
-              :collapsible="true"
-              @sizeChange="sizeChange"
-              @collapsedChange="collapsedChange"
-            >
+            <d-splitter-pane collapseDirection="before" :size="size" :minSize="minSize" :maxSize="maxSize" :collapsible="true" @sizeChange="sizeChange" @collapsedChange="collapsedChange">
               <Menu v-model="currentMenu"></Menu>
             </d-splitter-pane>
             <d-splitter-pane>
               <div class="pane-content">
-                <FileList
-                  :currentMenu="currentMenu"
-                  @openModal="openModal"
-                  :workspace="workSpaceFolder"
-                  :addFileTimes="addFileTimes"
-                ></FileList>
+                <FileList :currentMenu="currentMenu" @openModal="openModal" :workspace="workSpaceFolder" :addFileTimes="addFileTimes"></FileList>
                 <footer>
-                  <d-button variant="solid" icon="help" @click="handleHelper"
-                    >帮助</d-button
-                  >
-                  <d-button variant="solid" icon="exit" @click="handleClose"
-                    >退出</d-button
-                  >
+                  <d-button variant="solid" icon="help" @click="handleHelper">帮助</d-button>
+                  <d-button variant="solid" icon="exit" @click="handleClose">退出</d-button>
                 </footer>
               </div>
             </d-splitter-pane>
@@ -89,77 +43,70 @@
       </section>
     </content>
   </d-layout>
-  <FileModal
-    v-model="visible"
-    :currentMenu="currentMenu"
-    :currentWorkSpace="workSpaceFolder"
-    @update:currentWorkSpace="updateList"
-  ></FileModal>
+  <FileModal v-model="visible" :currentMenu="currentMenu" :currentWorkSpace="workSpaceFolder" @update:currentWorkSpace="updateList"></FileModal>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
-import Menu from "./components/Menu.vue";
-import logo from "../../assets/logo.jpg";
-import FileList from "./components/FileList.vue";
-import { useStorage } from "@vueuse/core";
-import FileModal from "./components/FileModal.vue";
+import { ref, watch } from 'vue'
+import Menu from './components/Menu.vue'
+import logo from '../../assets/logo.jfif'
+import ccs1 from '../../assets/ccs1.png'
+import ccs2 from '../../assets/ccs2.png'
+import FileList from './components/FileList.vue'
+import { useStorage } from '@vueuse/core'
+import FileModal from './components/FileModal.vue'
 
-const orientation = ref("horizontal");
+const orientation = ref('horizontal')
 const sourceBook = ref({
-  "Suction Installed Caisson Foundations for Offshore Wind: Design Guidelines [Offshore Wind Accelerator]":
-    "2019-suction-installed-caisson-foundations-for-of.pdf",
-  "Petroleum and natural gas industries-Specific requirements for offshore structures [ISO/DIS 19901-4]":
-    "ISODIS-19901-4-2022.pdf",
-});
+  'Suction Installed Caisson Foundations for Offshore Wind: Design Guidelines [Offshore Wind Accelerator]': '2019-suction-installed-caisson-foundations-for-of.pdf',
+  'Petroleum and natural gas industries-Specific requirements for offshore structures [ISO/DIS 19901-4]': 'ISODIS-19901-4-2022.pdf',
+})
 
-const splitBarSize = "2px";
+const splitBarSize = '2px'
 // splitter pane input
-const size = ref("25%");
-const minSize = ref("20%");
-const maxSize = ref("50%");
-const sizeChange = (size: number) => {};
-const collapsedChange = (event: Event) => {};
+const size = ref('25%')
+const minSize = ref('20%')
+const maxSize = ref('50%')
+const sizeChange = (size: number) => {}
+const collapsedChange = (event: Event) => {}
 
-const workSpaceFolder = useStorage("workspace-folder", "");
-const loadingState = ref(false);
+const workSpaceFolder = useStorage('workspace-folder', '')
+const loadingState = ref(false)
 const changeWorkSpace = async (e: Event) => {
-  loadingState.value = true;
-  const filePath = await (window as any).electronAPI.openFile(
-    workSpaceFolder.value
-  );
-  loadingState.value = false;
-  workSpaceFolder.value = filePath;
-};
+  loadingState.value = true
+  const filePath = await (window as any).electronAPI.openFile(workSpaceFolder.value)
+  loadingState.value = false
+  workSpaceFolder.value = filePath
+}
 
-const currentMenu = ref("");
+const currentMenu = ref('')
 
-const visible = ref(false);
+const visible = ref(false)
 
 const openModal = () => {
-  visible.value = true;
-};
+  visible.value = true
+}
 
-const addFileTimes = ref(0);
+const addFileTimes = ref(0)
 const updateList = (newPath: string) => {
-  console.log(newPath);
-  addFileTimes.value++;
-};
+  console.log(newPath)
+  addFileTimes.value++
+}
 
 const handleClose = () => {
-  (window as any).electronAPI.closeMain();
-};
+  ;(window as any).electronAPI.closeMain()
+}
 
 const openReference = async (v: any) => {
-  loadingState.value = true;
-  const res = await (window as any).electronAPI.openReferenceFile(v);
-  loadingState.value = false;
-  console.log(res);
-};
+  loadingState.value = true
+  const res = await (window as any).electronAPI.openReferenceFile(v)
+  loadingState.value = false
+  console.log(res)
+}
 
 const handleHelper = () => {
-  (window as any).electronAPI.openHelperFile("helper.pdf");
-};
+  ;(window as any).electronAPI.openHelperFile('helper.pdf')
+}
 </script>
 
 <style scoped>
@@ -198,10 +145,28 @@ const handleHelper = () => {
   margin-left: 7px;
 }
 
-.dheader .logo {
+.dheader .logo-wrap {
   height: 70%;
   margin-left: 30px;
   margin-right: 10px;
+  display: flex;
+}
+
+.dheader .ccs-wrap {
+  height: 100%;
+  margin-right: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.dheader .logo {
+  height: 100%;
+}
+
+.dheader .ccs-wrap .logo {
+  flex: 1;
 }
 
 .dcontent {
--- src/views/Home/components/FileModal.vue
+++ src/views/Home/components/FileModal.vue
@@ -3,35 +3,14 @@
     <template #header>
       <d-modal-header>
         <d-icon name="add-file"></d-icon>
-        <span style="vertical-align: text-bottom; margin-left: 5px"
-          >添加新的计算</span
-        >
+        <span style="vertical-align: text-bottom; margin-left: 5px">添加新的计算</span>
       </d-modal-header>
     </template>
-    <d-form
-      ref="formRef"
-      layout="vertical"
-      :data="{ workspace, filename }"
-      :pop-position="['right']"
-    >
-      <d-form-item
-        field="workspace"
-        :rules="[
-          { required: true, message: '文件名不能为空', trigger: 'blur' },
-        ]"
-        :show-feedback="false"
-        label="工作目录"
-      >
+    <d-form ref="formRef" layout="vertical" :data="{ workspace, filename }" :pop-position="['right']">
+      <d-form-item field="workspace" :rules="[{ required: true, message: '请于主界面选择工作目录', trigger: 'blur' }]" :show-feedback="false" label="工作目录">
         <d-input v-model="workspace" disabled placeholder="未选择工作目录" />
       </d-form-item>
-      <d-form-item
-        field="filename"
-        :rules="[
-          { required: true, message: '文件名不能为空', trigger: 'blur' },
-        ]"
-        :show-feedback="false"
-        label="文件名"
-      >
+      <d-form-item field="filename" :rules="[{ required: true, message: '文件名不能为空', trigger: 'blur' }]" :show-feedback="false" label="文件名">
         <d-input v-model="filename">
           <template #append>
             <span>.xlsx</span>
@@ -49,10 +28,10 @@
 </template>
 
 <script setup lang="ts">
-import { useVModel } from "@vueuse/core";
-import { getCurrentInstance, ref } from "vue";
+import { useVModel } from '@vueuse/core'
+import { getCurrentInstance, ref } from 'vue'
 
-const formRef = ref(null);
+const formRef = ref(null)
 const props = defineProps({
   modelValue: {
     required: true,
@@ -66,37 +45,28 @@ const props = defineProps({
     required: true,
     type: String,
   },
-});
-const emit = defineEmits(["update:modelValue", "updata:currentWorkSpace"]);
+})
+const emit = defineEmits(['update:modelValue', 'updata:currentWorkSpace'])
 
-const visible = useVModel(props, "modelValue", emit);
-const filename = ref("");
-const workspace = useVModel(props, "currentWorkSpace", emit);
-const message =
-  getCurrentInstance()?.appContext.config.globalProperties.$message;
+const visible = useVModel(props, 'modelValue', emit)
+const filename = ref('')
+const workspace = useVModel(props, 'currentWorkSpace', emit)
+const message = getCurrentInstance()?.appContext.config.globalProperties.$message
 
 const hidden = () => {
-  visible.value = false;
-};
+  visible.value = false
+}
 const handleConfirm = () => {
-  (formRef.value as any).validate(async (isValid: any, invalidFields: any) => {
-    if (!isValid) return;
-    const res = await (window as any).electronAPI.readCalcFile(
-      workspace.value,
-      filename.value + ".xlsx"
-    );
+  ;(formRef.value as any).validate(async (isValid: any, invalidFields: any) => {
+    if (!isValid) return
+    const res = await (window as any).electronAPI.readCalcFile(workspace.value, filename.value + '.xlsx')
     if (res) {
-      message({ type: "warning", message: "文件名重复" });
-      return;
+      message({ type: 'warning', message: '文件名重复' })
+      return
     }
-    await (window as any).electronAPI.openCalcWin(
-      props.currentMenu,
-      workspace.value,
-      filename.value + ".xlsx",
-      "calc"
-    );
-    visible.value = false;
-    workspace.value = workspace.value;
-  });
-};
+    await (window as any).electronAPI.openCalcWin(props.currentMenu, workspace.value, filename.value + '.xlsx', 'calc')
+    visible.value = false
+    workspace.value = workspace.value
+  })
+}
 </script>
